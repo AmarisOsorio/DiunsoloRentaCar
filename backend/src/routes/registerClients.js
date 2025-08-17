2efb2defb5275eeab3306d09e5d70e589dfd6f06
ssedux <christhiansanchez2409@gmail.com>
2025-08-16 22:32:08 -0600

    pdfGenerator Created

--- backend/src/routes/registerClients.js
+++ backend/src/routes/registerClients.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-import express from "express";
-=======
 //Imports
 import express from "express"
->>>>>>> 914ffd672dbc9070e913403124830f7326b446db
 import registerClientsController from "../controllers/registerClientsController.js";
 
 //Router
 const router = express.Router();
-
-<<<<<<< HEAD
-// Register client route
-router.route("/").post(registerClientsController.registerClients);
-
-// Email verification route
-router.route("/verifyCodeEmail").post(registerClientsController.verifyEmail);
-
-// Resend verification email route
-router.route("/resendCodeEmail").post(registerClientsController.resendVerificationEmail);
-=======
 //Route
 router.route("/")
   .post(registerClientsController.registerClients)
@@ -28,7 +13,6 @@ router.route("/verifyCodeEmail")
   .post(registerClientsController.verifyEmail)
 router.route("/resendCodeEmail")
   .post(registerClientsController.resendVerificationEmail)
->>>>>>> 914ffd672dbc9070e913403124830f7326b446db
 
 //Export
 export default router;
